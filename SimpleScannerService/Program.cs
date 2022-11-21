6778f10d5aebc2087a345bfe980d5d27dfbd2b79
bjdavey <jbjdavey@hotmail.com>
2022-11-21 15:30:48 +0300

    --Windows EXE

--- SimpleScannerService/Program.cs
+++ SimpleScannerService/Program.cs
@@ -1,20 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var baseUrl = builder.Configuration.GetValue<string>("baseUrl");
 
-builder.Host.UseWindowsService(options =>
-{
-    options.ServiceName = "Simple.Scanner.Service";
-});
 builder.Services.AddControllers();
 
 if (!string.IsNullOrEmpty(baseUrl))
     builder.WebHost.UseUrls(baseUrl);
 
 var app = builder.Build();
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
 
 app.MapControllers();
 
